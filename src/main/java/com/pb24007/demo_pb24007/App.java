e8983a30ef3d6db7e39b9023315187fa6a005247
Josepbarahona <167605902+Josepbarahona@users.noreply.github.com>
2024-05-03 20:22:33 -0600

    Initial commit
    Initial commit

---
+++ src/main/java/com/pb24007/demo_pb24007/App.java
@@ -0,0 +1,19 @@
+package com.pb24007.demo_pb24007;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String Hello () {
+        return  "Hello World!";
+    }
+    
+    public static void main (String [ ] args)
+    {	
+	App hw = new App () ;
+	System.out.println (hw.Hello () );
+	}
+    
+}
---
+++ src/test/java/com/pb24007/demo_pb24007/AppTest.java
@@ -0,0 +1,20 @@
+package com.pb24007.demo_pb24007;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
